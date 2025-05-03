f061981455a91d0d781180c299c3e922ef8e816c
melvynmartinez861229 <melvynmartinez861229@gmail.com>
2025-05-03 09:00:20 -0400

    la familia en la educ

--- src/app/pages/lafamiliaenlaeducacion/lafamiliaenlaeducacion.page.ts
+++ src/app/pages/lafamiliaenlaeducacion/lafamiliaenlaeducacion.page.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonButtons,IonMenuButton,IonImg,IonList,IonItem,IonBadge,IonLabel } from '@ionic/angular/standalone';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonTitle, 
+  IonToolbar,
+  IonMenuButton,
+  IonButtons,
+  IonImg,
+  IonItem,
+  IonBadge,
+  IonList,
+  IonLabel,
+  IonIcon // <-- Agregar esta importación
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-lafamiliaenlaeducacion',
   templateUrl: './lafamiliaenlaeducacion.page.html',
-  styles: '',
+  styleUrls: ['./lafamiliaenlaeducacion.page.scss'],
   standalone: true,
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonImg,IonList,IonItem,IonBadge,IonLabel]
 })
