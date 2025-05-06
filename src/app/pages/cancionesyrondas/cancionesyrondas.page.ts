31398b992f87213e6c5e095923ef90c8c04c2a97
melvynmartinez861229 <melvynmartinez861229@gmail.com>
2025-05-06 03:36:36 -0400

    update

--- src/app/pages/cancionesyrondas/cancionesyrondas.page.ts
+++ src/app/pages/cancionesyrondas/cancionesyrondas.page.ts
@@ -1,7 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonButtons,IonMenuButton,IonImg,IonList,IonItem,IonBadge,IonLabel } from '@ionic/angular/standalone';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonTitle, 
+  IonToolbar,
+  IonMenuButton,
+  IonButtons,
+  IonImg,
+  IonItem,
+  IonBadge,
+  IonList,
+  IonLabel,
+  IonIcon // <-- Agregar esta importación
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-cancionesyrondas',
